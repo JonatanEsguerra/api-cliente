36e6c95185cbdce4d96231999bbb1068ca8e7a4a
JonatanEsguerra <jonatanesgerra@gmail.com>
2026-08-26 19:49:53 -0500

    Implement CORS middleware in app.js
    Added CORS middleware to allow cross-origin requests.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.use(express.json());
 
+// CORS: necesario para que el cliente HTML (bono) pueda llamar a la API desde el navegador
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 // Rutas
 const clienteRoutes = require('./routes/clienteRoutes');
 app.use('/clientes', clienteRoutes);
